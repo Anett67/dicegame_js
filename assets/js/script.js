b2c06a19a514008d6abe8e6d00576ec4f674c04c
Anett David <davidanett@gmail.com>
2021-04-02 06:31:14 +0200

    Select DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Select elements
+const diceImage                 = document.querySelector('.dice-image');
+const gameBoardContainer        = document.querySelector('.gameboard');
+const currentPlayerGlobalScore  = document.querySelector('.player-block.current-player > .global-score');
+const currentPlayerRoundScore   = document.querySelector('.round-block.current-player > .round-score');;
+const newGameBtn                = document.getElementById('new-game');
+const rollDiceBtn               = document.getElementById('roll-dice');
+const holdBtn                   = document.getElementById('hold');
+
+
